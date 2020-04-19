76f9fbb1d7649414711629e1b9f2c9bcac38d54b
Mustafa Ersoy <40203872+mustafaersoyer@users.noreply.github.com>
2020-04-19 14:21:34 +0300

    Change GetAllData return type

--- app/src/main/java/com/example/codelesssdk/MainActivity.java
+++ app/src/main/java/com/example/codelesssdk/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.codeless.Codeless;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Codeless codeless = new Codeless();
+        codeless.GetAllData("denemeDB","characters");
     }
 }
--- codeless/src/main/java/com/example/codeless/Codeless.java
+++ codeless/src/main/java/com/example/codeless/Codeless.java
@@ -10,25 +10,25 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Codeless {
-    public void GetAllData(String dbName,String collectionName){
+    JsonObject movies;
+    public String GetAllData(String dbName,String collectionName){
         ApiInterface apiInterface =
                 ApiClient.getRetrofitInstance().create(ApiInterface.class);
-
 
         Call<JsonObject> call = apiInterface.getAllData(dbName,collectionName);
         call.enqueue(new Callback<JsonObject>() {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                JsonObject movies = response.body();
-                Log.d("tagg",movies.toString());
-              //  recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+                 movies = response.body();
+                Log.d("tagg", movies.toString());
             }
 
             @Override
             public void onFailure(Call<JsonObject> call, Throwable t) {
                 Log.d("Error", t.getMessage());
             }
-        });
 
+        });
+        return movies.toString();
     }
 }
